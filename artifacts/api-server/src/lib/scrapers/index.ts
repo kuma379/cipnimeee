3921396f02fe933ed74f9efd5e8ba2eb69d8db6f
kuma379 <145721266+kuma379@users.noreply.github.com>
2026-03-28 18:18:34 +0700

    Upload artifacts/api-server/src/lib/scrapers/index.ts

---
+++ artifacts/api-server/src/lib/scrapers/index.ts
@@ -0,0 +1,8 @@
+export * as otakudesu from "./otakudesu.js";
+export * as samehadaku from "./samehadaku.js";
+export * as kusonime from "./kusonime.js";
+export * as anoboy from "./anoboy.js";
+
+export type Source = "otakudesu" | "samehadaku" | "kusonime" | "anoboy";
+
+export const SOURCES: Source[] = ["otakudesu", "samehadaku", "kusonime", "anoboy"];
